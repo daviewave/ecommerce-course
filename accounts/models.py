0a03334631101dfcf937a1d5e4fb7199d2bb20b4
daviewave <daviewave@gmail.com>
2022-11-26 13:16:12 -0500

    added Account and AccountManager models in new app called accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class AccountManager(BaseUserManager):
+    # NOTE: Function to create a user with no special permissions
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email or not username:
+            raise ValueError("Accounts must contain an email and a username")
+
+        # NOTE: 'normalize_email' is just making everything lowercase to create consistency in database
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save() # NOTE: this might be deprecated. Course is using django 3.2 -- this is just saving our newly created user into the database
+        return user
+
+    # NOTE: Function to create a user with special site access
+    def create_superuser(self, first_name, last_name, username, email, password):
+        superuser = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name =  last_name,
+        )
+
+        # Give the user superuser permissions
+        superuser.is_admin = True
+        superuser.is_active = True
+        superuser.is_staff = True
+        superuser.is_superadmin = True
+        superuser.save()
+        return superuser
+
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
@@ -20,5 +55,16 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
+    # NOTE: This lets the account model know that they will be using/inheriting the Account Models to instantiate user and superuser accounts
+    objects = AccountManager()
+
+    # NOTE: This causes the top field on the django login page to prompt for email instead of username
     def __str__(self):
         return self.email
+
+    # django required methods
+    def has_perm(self, permission, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
