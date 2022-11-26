330fc41a4b605b691391ffac30090779df11ffcb
daviewave <daviewave@gmail.com>
2022-11-26 10:35:09 -0500

    added the category model for the different categories of stuff we will be selling on the ecommerce site

---
+++ category/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(
+        max_length=100, unique=True
+    )  # slug is the url of the category
+    description = models.CharField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to="photos/categories", blank=True)
+
+    # This controls the display name on the admin page (more so the lower one i believe)
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    # THIS IS STRICTLY DONE FOR ADMIN PAGE
+    def __str__(self):
+        return self.category_name
