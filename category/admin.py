2d74449e4f2a5d8fcb478fc4c804140b1d6ff072
daviewave <daviewave@gmail.com>
2022-11-26 21:37:49 -0500

    updated category models so that slug is automatically filled in based on the input for category name

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Category
 
-# Register your models here.
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)} # NOTE: in python if we declare a tuple with only 1 value, it must have a comma at the end
+    list_display = ('category_name', 'slug')
+
+# Register models
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(
-        max_length=100, unique=True, blank=True
-    )  # slug is the url of the category
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     description = models.CharField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to="photos/categories", blank=True)
 
