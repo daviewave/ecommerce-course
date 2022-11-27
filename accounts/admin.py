832d18fbf123368f97e48eb6745f1db0659e1d15
daviewave <daviewave@gmail.com>
2022-11-26 20:47:37 -0500

    mainly about how to control whats displayed to the django admin UI, like the  models / indidual instances of models + code to make sure password is hidden on the django admin UI

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    # NOTE: 'list_display' controls the columns that are displayed inside the app on the django admin UI
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active')
+
+    # NOTE: 'list_display_links' controls the fields from list display that are clickable, and lead to more information
+    list_display_links = ('email', 'first_name', 'last_name')
+
+    # NOTE: 'readonly_fields' controls the admin fields that will not be allowed to be edited
+    readonly_fields = ('last_login', 'date_joined')
+
+    # NOTE: 'ordered' controls the order in which each instance of the model is listed on the django admin page
+    ordered = ('-date_joined')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+# register models
+admin.site.register(Account, AccountAdmin)
