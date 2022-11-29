8ff8fca936c75f49f2273796eea2f87530b3f5fe
daviewave <daviewave@gmail.com>
2022-11-29 18:59:24 -0500

    fixed formatting of code in categories app files

--- category/models.py
+++ category/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100, unique=True, blank=True)
-    description = models.CharField(max_length=255, blank=True)
-    cat_image = models.ImageField(upload_to="photos/categories", blank=True)
+    category_name   = models.CharField(max_length=50, unique=True)
+    slug            = models.SlugField(max_length=100, unique=True, blank=True)
+    description     = models.CharField(max_length=255, blank=True)
+    cat_image       = models.ImageField(upload_to="photos/categories", blank=True)
 
     # This controls the display name on the admin page (more so the lower one i believe)
     class Meta:
-        verbose_name = "category"
-        verbose_name_plural = "categories"
+        verbose_name            = "category"
+        verbose_name_plural     = "categories"
 
-    # THIS IS STRICTLY DONE FOR ADMIN PAGE
+    # This controls what is displayed to the admin page as the overall app display name (major categories on the left in django 4.1)
     def __str__(self):
         return self.category_name
