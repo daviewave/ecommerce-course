258328e93813d250a13af6306108427bbb13a741
daviewave <daviewave@gmail.com>
2022-11-29 20:44:38 -0500

    gave the products to the render function as context in BASE_DIR/views.py and looped through the products versus having html code for each

--- ecommerce_course/views.py
+++ ecommerce_course/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+#-- my imports --#
+from store.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True) # Only bring the available products
+    context = {
+        'products': products,
+    }
 
 
-def home(req):
-    return render(req, "home.html")
+    return render(request, "home.html", context)
