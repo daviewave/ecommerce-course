809d2314551caf5f37a81dc55b912b818405faee
daviewave <daviewave@gmail.com>
2022-11-29 22:25:27 -0500

    added code to get all the categories from the database and display them to the navbar menu

--- ecommerce_course/settings.py
+++ ecommerce_course/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                
+                #NOTE: By adding this, we are allowing all the templates to use this function
+                "category.context_processors.get_category_links"
             ],
         },
     },
