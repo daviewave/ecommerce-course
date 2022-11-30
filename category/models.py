ef5a92f7edd041f61e741a5337c3cf43921d6b4a
daviewave <daviewave@gmail.com>
2022-11-29 22:38:03 -0500

    added code to dynamically route the urls and added the links to the relevant locations

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,9 @@ class Category(models.Model):
     class Meta:
         verbose_name            = "category"
         verbose_name_plural     = "categories"
+
+    def get_url(self):
+            return reverse('products_by_category', args=[self.slug])
 
     # This controls what is displayed to the admin page as the overall app display name (major categories on the left in django 4.1)
     def __str__(self):
