eff33d6473d263e3cba53ec5e774671f2397a19f
daviewave <daviewave@gmail.com>
2022-12-04 12:00:08 -0500

    added endpoints to add to cart, remove from cart, and remove all items from cart

--- carts/views.py
+++ carts/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render, redirect, HttpResponse
 from store.models import Product
 from .models import Cart, CartItem
 
-# Create your views here.
-
 def _get_session_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
     return cart
 
-def add_to_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
-
+def _get_current_users_cart(request):
     try: 
         cart = Cart.objects.get(cart_id=_get_session_id(request))
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_get_session_id(request))
     cart.save()
+    return cart
 
+def _apply_tax(tax_percentage, total_amount):
+    return (tax_percentage * total_amount) / 100
+
+def _calculate_grand_total(total, tax):
+    return total + tax
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart = _get_current_users_cart(request)
+    
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1
@@ -30,9 +37,27 @@ def add_to_cart(request, product_id):
             cart=cart
         )
         cart_item.save()
+    return redirect('cart')
 
-    return HttpResponse(cart_item, product)
-    # return redirect('cart')
+def remove_from_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart = _get_current_users_cart(request)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else: 
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_all_cart_item(request, product_id):
+    cart = _get_current_users_cart(request)
+    product = Product.objects.get(id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try: 
@@ -41,6 +66,10 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        
+        tax = _apply_tax(2, total)
+        grand_total = _calculate_grand_total(total, tax)
+
     except Cart.DoesNotExist:
         pass
 
@@ -49,6 +78,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         "total": total,
         "quantity": quantity,
         "cart_items": cart_items,
+        "tax": tax,
+        "grand_total": grand_total,
     }
 
     return render(request, 'store/cart.html', context)
