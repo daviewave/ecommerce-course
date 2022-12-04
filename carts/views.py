83260a46abde0eba99c7b28330d7736f4f35a7fd
daviewave <daviewave@gmail.com>
2022-12-04 06:11:42 -0500

    created add_to_cart + _get_session_id functions to my cart/views.py

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from store.models import Product
+from .models import Cart, CartItem
 
 # Create your views here.
+
+def _get_session_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    try: 
+        cart = Cart.objects.get(cart_id=_get_session_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_get_session_id(request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product, 
+            quantity=1, 
+            cart=cart
+        )
+        cart_item.save()
+
+    return HttpResponse(cart_item, product)
+    # return redirect('cart')
+
 def cart(request):
     return render(request, 'store/cart.html')
