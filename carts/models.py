dfdcfdbcb0069afd6da4bbebf4301609f6c1c0c5
daviewave <daviewave@gmail.com>
2022-12-04 11:00:14 -0500

    added a function to the CartItem model to return the total amount for the product given the quantity the customer has

--- carts/models.py
+++ carts/models.py
@@ -18,5 +18,8 @@ class CartItem(models.Model):
     quantity    = models.IntegerField()
     is_active   = models.BooleanField(default=True)
 
+    def get_cart_item_total(self):
+        return self.product.price * self.quantity 
+
     def __str__(self):
         return str(self.product)
