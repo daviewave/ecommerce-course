d5d11c4d958223b6cc6a72ac52422291f801e361
daviewave <daviewave@gmail.com>
2022-12-11 16:36:35 -0500

    added custom fields to the RegistrationForm for password creation

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
-    class Meta:
+    password         = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Re-Type Password',
+        'class': 'form-control',
+    }))
+    
+    class Meta: 
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
