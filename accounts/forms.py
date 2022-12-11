dcbcc727f5a4fd59155812a7e1aa3047139b0ac6
daviewave <daviewave@gmail.com>
2022-12-11 17:25:01 -0500

    added a function to pass the password & password confirmationa around + used this to ensure that that the password and password_confirmation inputs are matching before creating the new user and displays the errors if any field values are already stored in the database

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,7 +14,7 @@ class RegistrationForm(forms.ModelForm):
     class Meta: 
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
-        
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder']   = 'Enter First Name'
@@ -24,4 +24,14 @@ class RegistrationForm(forms.ModelForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+    
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords entered do not match"
+            )
             
