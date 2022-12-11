10fe23e3227ca53f8bc720f2c60df7640ae40174
daviewave <daviewave@gmail.com>
2022-12-10 19:43:53 -0500

    added a admin class for variation

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 #-- my imports --#
-from .models import Product
+from .models import Product, Variation
 
 #NOTE: to populate the value for slug based on the input used for 'product_name' need to add the following class
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+
 
 # Register models
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
