ffe5fe2b6864ed00590ce0ef9f1474eef9a1c677
daviewave <daviewave@gmail.com>
2022-12-19 14:35:33 -0500

    updated the Cart model to have a new field that is a Foreign Key to the User that the Cart is associated with

--- carts/models.py
+++ carts/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from store.models import Product, Variation
+from accounts.models import Account
 
 # Create your models here.
 class Cart(models.Model):
     cart_id     = models.CharField(max_length=250, blank=True)
     date_added  = models.DateField(auto_now_add=True)
+
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.cart_id
@@ -12,7 +15,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     product     = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations  = models.ManyToManyField(Variation, blank=True)
-    cart        = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart        = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity    = models.IntegerField()
     is_active   = models.BooleanField(default=True)
 
