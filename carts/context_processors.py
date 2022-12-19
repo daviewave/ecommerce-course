e3ca45a8362663af0a5c6f79b552fc5ff5e9ae48
daviewave <daviewave@gmail.com>
2022-12-19 15:56:26 -0500

    updated the number displayed on the cart icon in the navbar to check for users cart items before sessions

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,6 +1,12 @@
 from .admin import admin
 from .models import Cart, CartItem
-from .views import _get_session_id
+# from .views import _get_session_id
+
+def _get_session_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
 
 def calculate_number_of_items(cart_items):
     num_items = 0
@@ -15,7 +21,11 @@ def shopping_cart_counter(request):
     else: 
         try: 
             cart = Cart.objects.filter(cart_id=_get_session_id(request))
-            cart_items = CartItem.objects.filter(cart=cart[:1])
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
+
             num_items = calculate_number_of_items(cart_items)
         except Cart.DoesNotExist:
             num_items = 0
