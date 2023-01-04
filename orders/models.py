5b166a35e87bbe927216d4000d68e227ac86aa12
daviewave <daviewave@gmail.com>
2023-01-04 14:43:47 -0500

    fixed the string that is returned to the admin page

--- orders/models.py
+++ orders/models.py
@@ -55,7 +55,7 @@ class Order(models.Model):
     payment         = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
-        return self.user.first_name
+        return self.first_name
     
 class OrderProduct(models.Model):
     # CharFields
