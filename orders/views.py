a5eea5a59253f6ad4ff3c32f67c6232c5659dfb8
daviewave <daviewave@gmail.com>
2023-01-04 13:08:00 -0500

    refactored code for place_order function view in orders

--- orders/views.py
+++ orders/views.py
@@ -20,61 +20,60 @@ def _apply_tax(tax_percentage, total_amount):
 def _calculate_grand_total(total, tax):
     return total + tax
 
+def _check_if_shopping_cart_empty(cart_items):
+    if cart_items.count() <= 0:
+        return redirect('store')
+    return
+
+def _collect_form_data(request, user, tax, grand_total):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        try:
+            if form.is_valid():
+                data = Order()
+                data.user = user
+                data.first_name     = form.cleaned_data['first_name']
+                data.last_name      = form.cleaned_data['last_name']
+                data.email          = form.cleaned_data['email']
+                data.phone          = form.cleaned_data['phone']
+                data.addr_1         = form.cleaned_data['addr_1']
+                data.addr_2         = form.cleaned_data['addr_2']
+                data.city           = form.cleaned_data['city']
+                data.state          = form.cleaned_data['state']
+                data.country        = form.cleaned_data['country']
+                data.order_note     = form.cleaned_data['order_note']
+
+                data.tax            = tax
+                data.order_total    = grand_total
+                data.ip_addr        = request.META.get('REMOTE_ADDR')
+                data.save()
+
+                # Generate order number
+                year            = int(datetime.date.today().strftime('%Y'))
+                month           = int(datetime.date.today().strftime('%m'))
+                day             = int(datetime.date.today().strftime('%d'))
+                today           = datetime.date(year, month, day)
+                current_date    = today.strftime("%Y%m%d")
+
+                order_number        = current_date + str(data.id)
+                data.order_number   = order_number
+
+                data.save()
+                # return redirect('checkout')
+        except Exception as invalid_form:
+            raise(invalid_form)
+            
 def place_order(request, total=0, quantity=0):
     user = request.user
     
     cart_items = CartItem.objects.filter(user=user)
-    num_items = cart_items.count()
+    _check_if_shopping_cart_empty(cart_items)
 
-    if num_items <= 0:
-        return redirect('store')
-
-    grand_total = 0
-    tax = 0
     total, quantity = _calculate_total_and_quantities(cart_items)
+    
+    grand_total, tax = 0, 0
     tax = _apply_tax(total, 2)
     grand_total = _calculate_grand_total(total, tax)
 
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        try:
-            form.is_valid()
-        except Exception as e:
-            raise(e)
-
-        if form.is_valid():
-            data = Order()
-            data.user = user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.email = form.cleaned_data['email']
-            data.phone = form.cleaned_data['phone']
-            data.addr_1 = form.cleaned_data['addr_1']
-            data.addr_2 = form.cleaned_data['addr_2']
-            data.city = form.cleaned_data['city']
-            data.state = form.cleaned_data['state']
-            data.country = form.cleaned_data['country']
-            data.order_note = form.cleaned_data['order_note']
-
-            data.tax = tax
-            data.order_total = grand_total
-            data.ip_addr = request.META.get('REMOTE_ADDR')
-            data.save()
-
-            # Generate order number
-            year = int(datetime.date.today().strftime('%Y'))
-            month = int(datetime.date.today().strftime('%m'))
-            day = int(datetime.date.today().strftime('%d'))
-            today = datetime.date(year, month, day)
-            current_date = today.strftime("%Y%m%d")
-
-            order_number = current_date + str(data.id)
-            data.order_number = order_number
-
-            data.save()
-            return redirect('checkout')
-        else:
-            print('TITTTTTTTTTTIESSSS')
-            return redirect('checkout')
-
-
+    _collect_form_data(request, user, tax, grand_total)
+    return redirect('checkout')
