e8f219a6de2745ef720dcca77272f172a165af94
daviewave <daviewave@gmail.com>
2023-01-08 16:28:52 -0500

    updated the admin display for the order models

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
-admin.site.register(Payment)
-admin.site.register(Order)
-admin.site.register(OrderProduct)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display    = ['user', 'full_name', 'payment', 'order_number', 'status', 'is_ordered']
+    list_filter     = ['status', 'is_ordered']
+    search_fields   = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page   = 20
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity', 'account', 'order', 'payment']
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['payment_id', 'user', 'payment_method', 'amount_paid', 'created_at', 'status']
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct, OrderProductAdmin)
+admin.site.register(Payment, PaymentAdmin) 
