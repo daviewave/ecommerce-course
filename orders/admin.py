226250351b02ca9cd8742505425b4199fdca718d
daviewave <daviewave@gmail.com>
2023-01-08 17:03:09 -0500

    changed OrderProductAdmin class to an inline admin model so the order products are displayed in model view within a selected order

--- orders/admin.py
+++ orders/admin.py
@@ -2,19 +2,22 @@ from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
 
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+    readonly_fields = ('payment', 'account', 'product', 'quantity', 'product_price', 'is_ordered')
+
 class OrderAdmin(admin.ModelAdmin):
     list_display    = ['user', 'full_name', 'payment', 'order_number', 'status', 'is_ordered']
     list_filter     = ['status', 'is_ordered']
     search_fields   = ['order_number', 'first_name', 'last_name', 'phone', 'email']
     list_per_page   = 20
+    inlines         = [OrderProductInline]
 
-class OrderProductAdmin(admin.ModelAdmin):
-    list_display = ['product', 'quantity', 'account', 'order', 'payment']
-
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['payment_id', 'user', 'payment_method', 'amount_paid', 'created_at', 'status']
+# class PaymentAdmin(admin.ModelAdmin):
+#     list_display = ['payment_id', 'user', 'payment_method', 'amount_paid', 'created_at', 'status']
 
 
 admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderProduct, OrderProductAdmin)
-admin.site.register(Payment, PaymentAdmin) 
+admin.site.register(OrderProduct)
+admin.site.register(Payment) 
