07aa54790e51d548688904f284e7d616a9688282
daviewave <daviewave@gmail.com>
2023-01-13 12:38:28 -0500

    refactored submit review view function to be modularized

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect
+from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _get_session_id
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
+from .forms import ReviewForm
+from django.contrib import messages
 
 #-- Helper Methods --#
 def _get_single_product(category_slug, product_slug):
@@ -65,3 +67,45 @@ def search(request):
         "num_product": num_products,
     }
     return render(request, 'store/store.html', context)
+
+def get_previous_page_url(request):
+    return request.META.get('HTTP_REFERER')
+
+def get_users_review(user, product_id):
+    return ReviewRating.objects.get(user__id=user, product__id=product_id)
+
+def get_review_instances(request, reviews):
+    return ReviewForm(request.POST, instance=reviews)
+
+def save_review_form_data(request, form, url, product_id):
+    data = ReviewRating()
+    data.subject = form.cleaned_data['subject']
+    data.rating = form.cleaned_data['rating']
+    data.review = form.cleaned_data['review']
+    data.ip = request.META.get('REMOTE_ADDR')
+    data.product_id = product_id
+    data.user_id = request.user.id
+    data.save()
+    messages.success(request, 'Thank you! Your review has been submitted')
+    return redirect(url)
+
+
+
+def handle_review_form_data(request, user, url, product_id):
+    try:
+        reviews = get_users_review(user, product_id)
+        form = get_review_instances(request, reviews)
+        form.save()
+        messages.success(request, 'Thank you! Your review has been updated')
+        return redirect(url)
+    except ReviewRating.DoesNotExist:
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            save_review_form_data(request, user, url, product_id)
+
+
+def submit_review(request, product_id):
+    url = get_previous_page_url(request)
+    if request.method == 'POST':
+        handle_review_form_data(request, request.user, url, product_id)
+
