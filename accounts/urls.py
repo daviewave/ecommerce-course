753da08a9c2c31e97108c29acb14af5824cb4b07
daviewave <daviewave@gmail.com>
2023-01-14 16:44:31 -0500

    added endpoint for my_orders

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/', views.reset_password, name='reset_password'),
+    path('my_orders/', views.my_orders, name='my_orders'),
+    # path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
