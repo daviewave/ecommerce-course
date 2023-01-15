f15d4cc6560d0ea14364091824c103d58ea8a8c0
daviewave <daviewave@gmail.com>
2023-01-15 00:41:53 -0500

    got the reviews on the home page

--- ecommerce_course/views.py
+++ ecommerce_course/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 #-- my imports --#
-from store.models import Product
+from store.models import Product, ReviewRating
+
+def _get_all_products_reviews(products):
+    for product in products:
+        return ReviewRating.objects.filter(product_id=product.id, status=True)
+    
+    return ReviewRating.objects.filter(product_id=product.id, status=True)
 
 def home(request):
-    products = Product.objects.filter(is_available=True) # Only bring the available products
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+    reviews = _get_all_products_reviews(products)
+    
     context = {
         'products': products,
+        'reviews': reviews,
     }
-
-
     return render(request, "home.html", context)
