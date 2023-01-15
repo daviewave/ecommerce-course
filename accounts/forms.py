afaeae5d9d3ffaa518e0539b8a49b29321ad2d5c
daviewave <daviewave@gmail.com>
2023-01-14 21:21:47 -0500

    added Account Form and User Profile Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Account
+from .models import Account, UserProfile
 
 class RegistrationForm(forms.ModelForm):
     password         = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -31,7 +31,32 @@ class RegistrationForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
-            raise forms.ValidationError(
-                "Passwords entered do not match"
-            )
+            raise forms.ValidationError("Passwords entered do not match")
             
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super(AccountForm, self).__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages = {'invalid':("Image files only")}, widget=forms.FileInput)
+    
+    class Meta:
+        model = UserProfile
+        fields = ('addr_1', 'addr_2', 'city', 'state', 'country', 'zip_code', 'profile_picture')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+        
+
