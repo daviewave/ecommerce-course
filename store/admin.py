e2b188b7c3bbb77224aadf637f2566febc830430
daviewave <daviewave@gmail.com>
2023-01-14 23:49:58 -0500

    added product gallery admin model

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 #-- my imports --#
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 #NOTE: to populate the value for slug based on the input used for 'product_name' need to add the following class
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    inlines = [ProductGalleryInline]
     
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
@@ -13,7 +20,10 @@ class VariationAdmin(admin.ModelAdmin):
     list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
 
 
+
+
 # Register models
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
