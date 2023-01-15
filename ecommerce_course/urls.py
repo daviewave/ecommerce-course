82d718d4d6bb5c9de9b2911527d3f2ec1250ed14
daviewave <daviewave@gmail.com>
2023-01-15 01:24:30 -0500

    changed the url pattern for admin to seperate for security -- used an r'' which means regular expression

--- ecommerce_course/urls.py
+++ ecommerce_course/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^secure_admin/', include(admin.site.urls)),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
